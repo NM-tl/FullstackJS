9b9ee8b8b1d56b4de2e7da79208775e4ff698e76
NM-tl <nikitam42015@gmail.com>
2024-02-09 17:08:57 +0200

    js render cards

---
+++ hw17/assets/renderCards.js
@@ -0,0 +1,33 @@
+import { store } from "./store.js";
+
+document.addEventListener('DOMContentLoaded', () => {
+    const productsContainer = document.querySelector('.products');
+    
+    if (productsContainer && store) {
+        const productsHTML = store.map(({ title, description, price, discount, img }) => `
+            <div class="w-full max-w-60 h-fit flex flex-col gap-6">
+                <img 
+                    src="${img}" 
+                    alt="product"
+                    class="h-full max-h-72 w-full"
+                />
+                <div class="flex flex-col gap-2">
+                    <h5 class="text-base font-semibold text-main">${title}</h5>
+                    <span class="text-secondary text-sm font-semibold">${description}</span>
+                    <div class="flex items-center gap-1">
+                        <span class="font-semibold text-muted">$${price}</span>
+                        <span class="font-semibold text-accent">$${discount}</span>
+                    </div>
+                    <div class="flex items-center gap-2">
+                        <span class="inline-flex items-center rounded-full bg-sky-500 px-2 py-2"></span>
+                        <span class="inline-flex items-center rounded-full bg-green-800 px-2 py-2"></span>
+                        <span class="inline-flex items-center rounded-full bg-orange-500 px-2 py-2"></span>
+                        <span class="inline-flex items-center rounded-full bg-green-800 px-2 py-2"></span>
+                    </div>
+                </div>
+            </div>
+        `).join(''); 
+
+        productsContainer.innerHTML = productsHTML;
+    }
+});
