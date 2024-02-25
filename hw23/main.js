ac18373a915e2aea2943ad0479c992e6d5ac0cf3
NM-tl <nikitam42015@gmail.com>
2024-02-25 14:20:47 +0200

    init Convert minutes func
    - inti time container
    - added minutes converter
    - created checks for the user

---
+++ hw23/main.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const hoursSpan = document.getElementById("hours");
+    const minutesSpan = document.getElementById("minutes");
+    
+    let userInput;
+    
+    while (true) {
+        userInput = prompt("Введіть кількість хвилин:");
+        if (userInput !== null && userInput.trim() !== "" && !isNaN(userInput) && userInput >= 0 && userInput <= 5999) {
+            break;
+        } else if (userInput === null) {
+            break;
+        } else {
+            alert("Будь ласка, введіть адекватне числове значення (від 0 до 5999 хвилин).");
+        }
+    }
+
+    function convertMinutesToTime(minutes) {
+        if (isNaN(minutes) || minutes < 0 || minutes > 5999) {
+            alert("Будь ласка, введіть адекватне числове значення (від 0 до 5999 хвилин).");
+            return;
+        }
+    
+        const hours = Math.floor(minutes / 60);
+        const remainingMinutes = minutes % 60;
+
+        hoursSpan.style.setProperty('--value', hours);
+        minutesSpan.style.setProperty('--value', remainingMinutes);
+    }
+
+    convertMinutesToTime(parseInt(userInput));
+});
