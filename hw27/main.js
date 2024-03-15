a86c9850426b965834abe5f4f0afa01ea3155b49
NM-tl <nikitam42015@gmail.com>
2024-03-15 21:03:41 +0200

    finish first task
    - add render
    - refactor functions
    - hadle alerts max\min temp

--- hw27/main.js
+++ hw27/main.js
@@ -1,4 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const weatherList = document.getElementById("weatherList");
+    const getMinTemp = document.getElementById("getMin");
+    const getMaxTemp = document.getElementById("getMax");
+
     const weatherForecast = {
         Monday: 25,
         Tuesday: 26,
@@ -8,30 +12,54 @@ document.addEventListener("DOMContentLoaded", () => {
         Saturday: 18,
         Sunday: 15,
     };
-    
+
     const getAverageTemperature = (weather) => {
         const temperatures = Object.values(weather);
         const sum = temperatures.reduce((acc, cur) => acc + cur, 0);
-        const average = sum / temperatures.length;
-        return parseFloat(average.toFixed());
+        return parseFloat((sum / temperatures.length).toFixed());
     };
     
-    const getMaxTemperature = (weather) => {
-        return Math.max(...Object.values(weather));
-    };
+    const getMaxTemperature = (weather) => Math.max(...Object.values(weather));
     
-    const getMinTemperature = (weather) => {
-        return Math.min(...Object.values(weather));
-    };
+    const getMinTemperature = (weather) => Math.min(...Object.values(weather));
+
+    const getAlert = (btn, func, weather) => {
+        btn.addEventListener("click", () => { alert(func(weather)) });
+    }
+    
+    const getShortWeekday = (day) => day.slice(0, 3);
     
     const formatWeekDaysString = (weather) => {
-        const days = Object.keys(weather);
-        return `(${days.join(" - ")})`;
-    };
+        const shortDays = Object.keys(weather).map(getShortWeekday);
+        return `(${shortDays.join(" - ")})`;
+    };    
+
+    const renderWeatherList = (obj) => {   
+        const keys = Object.keys(obj);
+        const lastKeyIndex = keys.length - 1;
     
+        for (const [index, [day, temperature]] of Object.entries(obj).entries()) {
+            const listItem = document.createElement("li");
+            listItem.classList.add("flex", "flex-col", "gap-2", "text-center", "font-semibold", "p-5");
+            if (index !== lastKeyIndex) {
+                listItem.classList.add("border-r", "border-black");
+            }
+            listItem.innerHTML = `
+                <span>${getShortWeekday(day)}</span>
+                <span class="text-2xl">${temperature}°C</span>
+            `;
+            weatherList.appendChild(listItem);
+        }
+    };
+        
     console.log(getAverageTemperature(weatherForecast));
     console.log(getMaxTemperature(weatherForecast));
     console.log(getMinTemperature(weatherForecast));
     console.log(formatWeekDaysString(weatherForecast));
-    
+
+    if (weatherList && getMinTemp && getMaxTemp) {
+        renderWeatherList(weatherForecast);
+        getAlert(getMinTemp, getMinTemperature, weatherForecast);
+        getAlert(getMaxTemp, getMaxTemperature, weatherForecast);
+    }    
 });
