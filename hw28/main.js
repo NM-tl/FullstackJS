c17d70453d93b96e3e54baf8d1fb3c18ac383b0e
NM-tl <nikitam42015@gmail.com>
2024-03-15 21:16:34 +0200

    init deepFreeze to protect childrens

--- hw28/main.js
+++ hw28/main.js
@@ -1,30 +1,43 @@
 document.addEventListener("DOMContentLoaded", () => {
     // Task 1
     {
-        const fileSystem = Object.freeze({
+        const deepFreeze = (obj) => {
+            Object.freeze(obj);
+            Object.values(obj).forEach(value => {
+                if (value && typeof value === 'object' && !Object.isFrozen(value)) {
+                    deepFreeze(value);
+                }
+            });
+        };
+        
+        const fileSystem = {
             name: "root",
             type: "folder",
             children: [
-             {
-              name: "folder1",
-              type: "folder",
-              children: [
-               { name: "file1.txt", type: "file" },
-               { name: "file2.txt", type: "file" }
-              ]
-             },
-             {
-              name: "folder2",
-              type: "folder",
-              children: [
-               { name: "file3.txt", type: "file" }
-              ]
-             }
+                {
+                    name: "folder1",
+                    type: "folder",
+                    children: [
+                        { name: "file1.txt", type: "file" },
+                        { name: "file2.txt", type: "file" }
+                    ]
+                },
+                {
+                    name: "folder2",
+                    type: "folder",
+                    children: [
+                        { name: "file3.txt", type: "file" }
+                    ]
+                }
             ]
-        });
-
+        };
+        
+        deepFreeze(fileSystem);
+        
         // test
         // fileSystem.name = "newName";
+        // fileSystem.children[0].name = "newName";
+        // console.log(fileSystem)
     }
 });
    
