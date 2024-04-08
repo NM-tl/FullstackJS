69e1f55c921789683060911842675c741efcfb0a
NM-tl <nikitam42015@gmail.com>
2024-04-09 01:44:31 +0300

    fix ux & added badge counter

--- hw32/main.js
+++ hw32/main.js
@@ -3,21 +3,29 @@
     document.addEventListener("DOMContentLoaded", () => {
         const categoriesList = document.querySelectorAll('#categories .card');
         const numberOfCategories = categoriesList.length;
+
+        if(categoriesList) {
+            console.log(`Number of Categories: ${numberOfCategories}\n`);
     
-        console.log(`Number of Categories: ${numberOfCategories}\n`);
-    
-        categoriesList.forEach(category => {
-            const categoryName = category.textContent.trim();
-            const categoryItems = category.querySelectorAll('ul li');
-            const numberOfItems = categoryItems.length;
-    
-            const longestElement = [...categoryItems]
-                .map(item => item.textContent.trim())
-                .reduce((currentLongestElement, currentElement) => currentLongestElement.length > currentElement.length ?
-                currentLongestElement : 
-                currentElement, '');
-    
-            console.log(`Category: ${categoryName}\nElements: ${numberOfItems}\nLongestElement: ${longestElement}\n`);
-        });
+            categoriesList.forEach(category => {
+                const categoryName = category.textContent.trim();
+                const categoryItems = category.querySelectorAll('ul li');
+                const numberOfItems = categoryItems.length;
+        
+                const longestElement = [...categoryItems]
+                    .map(item => item.textContent.trim())
+                    .reduce((currentLongestElement, currentElement) => currentLongestElement.length > currentElement.length ?
+                    currentLongestElement : 
+                    currentElement, '');
+        
+                console.log(`Category: ${categoryName}\nElements: ${numberOfItems}\nLongestElement: ${longestElement}\n`);
+
+                category.innerHTML += `
+                    <div class="badge badge-info gap-2 absolute right-5 top-3">
+                        Items: ${numberOfItems}
+                    </div>
+                `;          
+            });   
+        }   
     });
 }
