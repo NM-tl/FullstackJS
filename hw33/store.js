d10beaac1cda018809cb4b6b09ee006f04ae5514
Nikita Maiboroda <30575825+NM-tl@users.noreply.github.com>
2024-04-16 20:59:36 +0300

    HW33 (#32)
    * HW33
    
    - init store
    - init modal onClick - image
    - handle close modal (esc\btn\wrapper)
    
    * remove unused html

---
+++ hw33/store.js
@@ -0,0 +1,82 @@
+export const images = [
+    {
+        src: "https://image.api.playstation.com/vulcan/img/rnd/202010/2614/NVmnBXze9ElHzU6SmykrJLIV.png",
+        alt: "Bloodborne",
+        year: "2024"
+    },
+    {
+        src: "https://image.api.playstation.com/vulcan/ap/rnd/202312/0117/da083fa5e19458dd750aa8a6ea30ba10bac6f87074693df5.jpg",
+        alt: "The Last of Us Part II",
+        year: "2024"
+    },
+    {
+        src: "https://assets.nintendo.com/image/upload/c_fill,w_1200/q_auto:best/f_auto/dpr_2.0/ncom/software/switch/70010000027619/9989957eae3a6b545194c42fec2071675c34aadacd65e6b33fdfe7b3b6a86c3a",
+        alt: "Animal Crossing: New Horizons",
+        year: "2024"
+    },
+    {
+        src: "https://media.contentapi.ea.com/content/dam/apex-legends/common/breakout-patch-notes/s20-launch-map-worldsedge-r01-v01.png.adapt.crop16x9.431p.png",
+        alt: "Apex Legends",
+        year: "2024"
+    },
+    {
+        src: "https://cdn.akamai.steamstatic.com/steam/apps/570/capsule_616x353.jpg?t=1710346933",
+        alt: "Dota 2",
+        year: "2024"
+    },
+    {
+        src: "https://cdn.akamai.steamstatic.com/steam/apps/1613284/capsule_616x353.jpg?t=1703992150",
+        alt: "Forza Horizon 5",
+        year: "2024"
+    },
+    {
+        src: "https://image.api.playstation.com/vulcan/ap/rnd/202104/0517/9AcM3vy5t77zPiJyKHwRfnNT.png",
+        alt: "Hades",
+        year: "2024"
+    },
+    {
+        src: "https://image.api.playstation.com/vulcan/ap/rnd/202008/2111/kZuu7RcHultdoVUuGsReuGcq.png",
+        alt: "Control",
+        year: "2024"
+    },
+    {
+        src: "https://zelda.nintendo.com/breath-of-the-wild/assets/icons/BOTW-Share_icon.jpg",
+        alt: "The Legend of Zelda: Breath of the Wild",
+        year: "2024"
+    },
+    {
+        src: "https://cdn.akamai.steamstatic.com/steam/apps/2420110/capsule_616x353.jpg?t=1711473066",
+        alt: "Horizon Forbidden West",
+        year: "2024"
+    },
+    {
+        src: "https://cdn.akamai.steamstatic.com/steam/apps/814380/capsule_616x353.jpg?t=1678991267",
+        alt: "Sekiro: Shadows Die Twice",
+        year: "2024"
+    },
+    {
+        src: "https://cdn.akamai.steamstatic.com/steam/apps/2215430/capsule_616x353.jpg?t=1712313893",
+        alt: "Ghost of Tsushima",
+        year: "2024"
+    },
+    {
+        src: "https://image.api.playstation.com/vulcan/img/rnd/202010/2217/p3pYq0QxntZQREXRVdAzmn1w.png",
+        alt: "God of War (2018)",
+        year: "2024"
+    },
+    {
+        src: "https://image.api.playstation.com/vulcan/ap/rnd/202311/2812/ae84720b553c4ce943e9c342621b60f198beda0dbf533e21.jpg",
+        alt: "Cyberpunk 2077",
+        year: "2024"
+    },
+    {
+        src: "https://upload.wikimedia.org/wikipedia/ru/0/02/Thief_box_art.jpg",
+        alt: "Thief",
+        year: "2024"
+    },
+    {
+        src: "https://cdn.akamai.steamstatic.com/steam/apps/892970/header.jpg?t=1708348390",
+        alt: "Valheim",
+        year: "2024"
+    }
+];
